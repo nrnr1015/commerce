58127cfc6d17745c9d65d140b63a00bf47e14c80
nrnr1015 <llr2916@gentlemonster.com>
2023-05-22 18:11:49 +0900

    설정

---
+++ my-commerce/components/Button.tsx
@@ -0,0 +1,7 @@
+
+export default function Button(params:type) {
+    const handleClick = () => {
+        alert('handleClickhandleClick')
+    }
+    return <button onClick={handleClick}>handleClick</button>
+}
---
+++ my-commerce/pages/index.tsx
@@ -0,0 +1,13 @@
+import Button from "@/components/Button";
+
+export default function Home() {
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <Button />
+
+      
+    </main>
+  )
+}
